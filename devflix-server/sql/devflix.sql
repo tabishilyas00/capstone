192cfeda8b2265a51b8faa06625c1ce5f7e7e1e1
Tabish Ilyas <tabish.ilyas18@gmail.com>
2023-10-17 15:27:34 -0500

    setup devflix database

--- devflix-server/sql/devflix.sql
+++ devflix-server/sql/devflix.sql
@@ -9,9 +9,63 @@ create table movie (
     rating varchar(10) not null,
     run_time varchar(5) not null,
     poster varchar(1000) not null,
-    director varchar(100),
-    writer varchar(100),
-    stars varchar(100),
-    country varchar(50),
-    `language` varchar(50)
+    country varchar(50) not null,
+    `language` varchar(50) not null
+);
+
+create table director (
+    director_id int primary key auto_increment,
+    `name` varchar(50) not null,
+    imageURL varchar(1000) null
+);
+
+create table writer (
+    writer_id int primary key auto_increment,
+    `name` varchar(50) not null,
+    imageURL varchar(1000) null
+);
+
+create table actor (
+    actor_id int primary key auto_increment,
+    `name` varchar(50) not null,
+    imageURL varchar(1000) null
+);
+
+create table movie_director (
+    movie_id int not null,
+    director_id int not null,
+    constraint pk_movie_director
+        primary key (movie_id , director_id),
+    constraint fk_movie_director_movie_id
+        foreign key (movie_id)
+        references movie(movie_id),
+    constraint fk_movie_director_director_id
+        foreign key (director_id)
+        references director(director_id)
+);
+
+create table movie_writer (
+    movie_id int not null,
+    writer_id int not null,
+    constraint pk_movie_writer
+        primary key (movie_id , writer_id),
+    constraint fk_movie_writer_movie_id
+        foreign key (movie_id)
+        references movie(movie_id),
+    constraint fk_movie_writer_writer_id
+        foreign key (writer_id)
+        references writer(writer_id)
+);
+
+create table movie_actor (
+    movie_id int not null,
+    actor_id int not null,
+    constraint pk_movie_actor
+        primary key (movie_id , actor_id),
+    constraint fk_movie_actor_movie_id
+        foreign key (movie_id)
+        references movie(movie_id),
+    constraint fk_movie_actor_actor_id
+        foreign key (actor_id)
+        references actor(actor_id)
 );
