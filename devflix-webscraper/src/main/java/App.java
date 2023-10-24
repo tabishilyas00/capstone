9ab4a92ddd6fd80016d810bc86ac5f107a280984
Tabish Ilyas <tabish.ilyas18@gmail.com>
2023-10-23 22:40:16 -0500

    describe what each scraper does

--- devflix-webscraper/src/main/java/App.java
+++ devflix-webscraper/src/main/java/App.java
@@ -5,6 +5,14 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws IOException, SQLException {
-        MovieLanguageScraper.scrape();
+//        MovieScraper.scrape();            Populates movie table
+//        PersonScraper.scrape();           Populates person table
+//        CountryScraper.scrape();          Populates country table
+//        LanguageScraper.scrape();         Populates lang table
+//        DirectorScraper.scrape();         Populates movie_director bridge table
+//        WriterScraper.scrape();           Populates movie_writer bridge table
+//        StarScraper.scrape();             Populates movie_star bridge table
+//        MovieCountryScraper.scrape();     Populates movie_country bridge table
+//        MovieLanguageScraper.scrape();    Populates movie_language bridge table
     }
 }
