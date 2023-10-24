a68616eb8547f6abab1bfde9ff6c50585e162830
Tabish Ilyas <tabish.ilyas18@gmail.com>
2023-10-23 22:35:26 -0500

    populated country and language bridge tables

--- devflix-webscraper/src/main/java/webscrapers/MovieCountryScraper.java
+++ devflix-webscraper/src/main/java/webscrapers/MovieCountryScraper.java
@@ -6,12 +6,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieCountryScraper {
     public static void scrape() throws IOException, SQLException {
+        Connection conn = DriverManager.getConnection(System.getenv("DB_URL") , System.getenv("DB_USERNAME") , System.getenv("DB_PASSWORD"));
+
         final String url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250";
         final String imdbURL = "https://www.imdb.com";
 
@@ -36,11 +38,80 @@ public class MovieCountryScraper {
                 }
             }
 
-            System.out.println("Title   : " + title);
+            int movieID = findMovieID(title , conn);
+
+            System.out.printf("Movie ID : %d%n" , movieID);
+            System.out.println("Title    : " + title);
             for (Element c : countryList) {
-                System.out.println("Country : " + c.text());
+                int countryID = findCountryID(c.text() , conn);
+
+                System.out.printf("Person ID : %d%n" , countryID);
+                System.out.println("Country   : " + c.text());
+
+                addCountry(movieID , countryID , conn);
             }
             System.out.println();
         }
+
+        conn.close();
+    }
+
+    public static int findMovieID(String title , Connection conn) throws SQLException {
+        final String sql = """
+                select
+                    movie_id
+                from movie
+                where title = ?;
+                """;
+
+        PreparedStatement pstmt =
+                conn.prepareStatement(sql);
+        pstmt.setString(1, title);
+
+        ResultSet rs = pstmt.executeQuery();
+        int movieID = 0;
+        while (rs.next()) {
+            movieID = rs.getInt("movie_id");
+        }
+
+        rs.close();
+        pstmt.close();
+        return movieID;
+    }
+
+    public static int findCountryID(String name , Connection conn) throws SQLException {
+        final String sql = """
+                select
+                    country_id
+                from country
+                where name = ?;
+                """;
+
+        PreparedStatement pstmt =
+                conn.prepareStatement(sql);
+        pstmt.setString(1, name);
+
+        ResultSet rs = pstmt.executeQuery();
+        int movieID = 0;
+        while (rs.next()) {
+            movieID = rs.getInt("country_id");
+        }
+
+        rs.close();
+        pstmt.close();
+        return movieID;
+    }
+
+    public static void addCountry(int movieID , int countryID , Connection conn) throws SQLException {
+        final String sql = """
+                insert into movie_country (movie_id , country_id) values (? , ?)
+                """;
+
+        PreparedStatement pstmt =
+                conn.prepareStatement(sql);
+        pstmt.setString(1, String.valueOf(movieID));
+        pstmt.setString(2, String.valueOf(countryID));
+        pstmt.executeUpdate();
+        pstmt.close();
     }
 }
--- devflix-webscraper/src/main/java/webscrapers/MovieLanguageScraper.java
+++ devflix-webscraper/src/main/java/webscrapers/MovieLanguageScraper.java
@@ -6,16 +6,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class MovieLanguageScraper {
     public static void scrape() throws IOException, SQLException {
+        Connection conn = DriverManager.getConnection(System.getenv("DB_URL") , System.getenv("DB_USERNAME") , System.getenv("DB_PASSWORD"));
+
         final String url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250";
         final String imdbURL = "https://www.imdb.com";
 
@@ -40,11 +38,80 @@ public class MovieLanguageScraper {
                 }
             }
 
+            int movieID = findMovieID(title , conn);
+
+            System.out.printf("Movie ID : %d%n" , movieID);
             System.out.println("Title    : " + title);
             for (Element l : languageList) {
-                System.out.println("Language : " + l.text());
+                int languageID = findLanguageID(l.text() , conn);
+
+                System.out.printf("Person ID : %d%n" , languageID);
+                System.out.println("Language  : " + l.text());
+
+                addLanguage(movieID , languageID , conn);
             }
             System.out.println();
         }
+
+        conn.close();
+    }
+
+    public static int findMovieID(String title , Connection conn) throws SQLException {
+        final String sql = """
+                select
+                    movie_id
+                from movie
+                where title = ?;
+                """;
+
+        PreparedStatement pstmt =
+                conn.prepareStatement(sql);
+        pstmt.setString(1, title);
+
+        ResultSet rs = pstmt.executeQuery();
+        int movieID = 0;
+        while (rs.next()) {
+            movieID = rs.getInt("movie_id");
+        }
+
+        rs.close();
+        pstmt.close();
+        return movieID;
+    }
+
+    public static int findLanguageID(String name , Connection conn) throws SQLException {
+        final String sql = """
+                select
+                    language_id
+                from lang
+                where name = ?;
+                """;
+
+        PreparedStatement pstmt =
+                conn.prepareStatement(sql);
+        pstmt.setString(1, name);
+
+        ResultSet rs = pstmt.executeQuery();
+        int movieID = 0;
+        while (rs.next()) {
+            movieID = rs.getInt("language_id");
+        }
+
+        rs.close();
+        pstmt.close();
+        return movieID;
+    }
+
+    public static void addLanguage(int movieID , int languageID , Connection conn) throws SQLException {
+        final String sql = """
+                insert into movie_language (movie_id , language_id) values (? , ?)
+                """;
+
+        PreparedStatement pstmt =
+                conn.prepareStatement(sql);
+        pstmt.setString(1, String.valueOf(movieID));
+        pstmt.setString(2, String.valueOf(languageID));
+        pstmt.executeUpdate();
+        pstmt.close();
     }
 }
